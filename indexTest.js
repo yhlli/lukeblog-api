64714e2576a1c7b0219c698eb8c68c1915938a15
Yiheng Li <yiheng.li@hcl.com>
2024-03-22 20:00:04 -0400

    added user profiles

--- indexTest.js
+++ indexTest.js
@@ -24,6 +24,56 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
+const authenticate = (req, res, next) => {
+    const accessToken = req.headers['authorization'];
+    const refreshToken = req.cookies['refreshToken'];
+  
+    if (!accessToken && !refreshToken) {
+      return res.status(401).send('Access Denied. No token provided.');
+    }
+    
+    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, {}, (err,info)=>{
+        if (err){
+            if (!refreshToken){
+                return res.status(401).sent('Access Denied. No refresh token provided.');
+            }
+            jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, {}, (err,info)=>{
+                if (err){
+                    return res.status(400).send('Invalid Token.')
+                }
+                const accessToken = jwt.sign( {info,id: info._id}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+                res
+                    .cookie('refreshToken', refreshToken, { httpOnly: true, sameSite: 'strict' })
+                    .header('authorization', accessToken)
+                    .send(info._id);
+            })
+        }
+        req.authInfo = info;
+        next();
+    })
+    /* try {
+      const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+      req.user = decoded.user;
+      next();
+    } catch (error) {
+      if (!refreshToken) {
+        return res.status(401).send('Access Denied. No refresh token provided.');
+      }
+  
+      try {
+        const decoded = jwt.verify(refreshToken, secretKey);
+        const accessToken = jwt.sign({ user: decoded.user }, secretKey, { expiresIn: '1h' });
+  
+        res
+          .cookie('refreshToken', refreshToken, { httpOnly: true, sameSite: 'strict' })
+          .header('Authorization', accessToken)
+          .send(decoded.user);
+      } catch (error) {
+        return res.status(400).send('Invalid Token.');
+      }
+    } */
+};
+
 try {
     mongoose.connect(process.env.DATABASE_URI);
     console.log('Connected to mongoose');
@@ -55,28 +105,19 @@ app.post('/login', async (req,res)=>{
         passOk = bcrypt.compareSync(password, userDoc.password);
     }
     if (passOk){
-        //Logged in
-        /* jwt.sign({username,id:userDoc._id}, process.env.ACCESS_TOKEN_SECRET, {}, (err,token) => {
-            if (err) throw err;
-            res.cookie('token', token).json({
-              id:userDoc._id,
-              username,
-            });
-          }); */
-        const accessToken = jwt.sign({username,id:userDoc._id}, process.env.ACCESS_TOKEN_SECRET, {});
+        const accessToken = jwt.sign({username,id:userDoc._id}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+        const refreshToken = jwt.sign({username,id:userDoc._id}, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
         try {
-            res.cookie('token', accessToken, {
-                httpOnly: false,
-                secure: true,
-                sameSite: 'none',
-            }).json({
+            res
+                .cookie('refreshToken', refreshToken, { httpOnly: true, sameSite: 'strict' })
+                .header('authorization', accessToken)
+                .json({
                 id:userDoc._id,
                 username,
             });
         } catch (error) {
             throw error;
         }
-        /* res.json(username); */
     } else{
         res.status(400).json('Wrong credentials');
     }
@@ -99,7 +140,7 @@ app.post('/logout', (req,res)=>{
     res.cookie('token', '').json('ok');
 });
 
-app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
+app.post('/post', uploadMiddleware.single('file'), authenticate, async (req,res) => {
     //const {originalname,path} = req.file;
     var path;
     var originalname;
@@ -118,21 +159,17 @@ app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
     //const ext = parts[parts.length - 1];
     const newPath = path+'.'+ext;
     if (req.file !== undefined) fs.renameSync(path, newPath);
-
-    const {token} = req.cookies;
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, {}, async (err,info)=>{
-        if(err) throw err;
+    const {_id} = req.headers;
             const {title,summary,content} = req.body;
             const postDoc = await Post.create({
                 title,
                 summary,
                 content,
                 cover:newPath,
-                author:info.id,
+                author:_id,
             });
-        res.json(postDoc);
+        //res.json(postDoc);
         //res.json({files:req.file})
-    });
 });
 
 app.put('/post', uploadMiddleware.single('file'), async (req,res) => {
@@ -190,7 +227,7 @@ app.delete('/post/:id', async(req,res)=>{
     await Comment.deleteMany({ postId: id });
     await Post.deleteOne(Post.findById(id));
     res.json('ok');
-})
+});
 
 app.post('/comment/:id', uploadMiddleware.single('file'), async(req,res)=>{
     const {id} = req.params;
@@ -211,19 +248,54 @@ app.get('/comment/:id', async(req,res)=>{
     const {id} = req.params;
     const commentDoc = await Comment.find({postId: id}).populate('author', ['username']);
     res.json(commentDoc);
-})
+});
 
 app.delete('/comment/:id', async(req,res)=>{
     const {id} = req.params;
     await Comment.deleteOne(Comment.findById(id));
     res.json('ok');
-})
+});
 
 app.listen(4000);
 
 app.get('/user/:id', (req,res)=>{
     const {id} = req.params;
     res.json(req.params);
-})
+});
 
 //Below may not work yet and is not integrated well
+
+
+
+/* app.post('/login', (req, res) => {
+    const user = {
+    id: 1,
+    username: 'john.doe'
+    };
+    
+    const accessToken = jwt.sign({ user }, secretKey, { expiresIn: '1h' });
+    const refreshToken = jwt.sign({ user }, secretKey, { expiresIn: '1d' });
+    
+    res
+    .cookie('refreshToken', refreshToken, { httpOnly: true, sameSite: 'strict' })
+    .header('Authorization', accessToken)
+    .send(user);
+}); */
+
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.cookies['refreshToken'];
+    if (!refreshToken) {
+      return res.status(401).send('Access Denied. No refresh token provided.');
+    }
+  
+    try {
+      const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+      const accessToken = jwt.sign({ user: decoded.user }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+  
+      res
+        .header('authorization', accessToken)
+        .send(decoded.user);
+    } catch (error) {
+      return res.status(400).send('Invalid refresh token.');
+    }
+});
