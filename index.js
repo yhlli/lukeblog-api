7bb1cc060934ad3a83d2c88b8b410907df4442da
Luke Li <36483872+yhlli@users.noreply.github.com>
2024-03-27 20:09:57 -0400

    fix

--- index.js
+++ index.js
@@ -101,13 +101,7 @@ app.post('/login', async (req,res)=>{
 });
 
 app.get('/profile', authenticate, (req,res)=>{
-    const {authorization} = req.cookies;
-    if (authorization === ''){
-        res.json('not logged in');
-    } else{
-        res.json(req.infoId);
-    }
-    
+    res.json(req.infoId);
 });
 
 app.post('/logout', (req,res)=>{
