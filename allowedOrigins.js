281de87fdf3c85b153717698123447c64a164823
Luke Li <36483872+yhlli@users.noreply.github.com>
2024-05-31 17:29:05 -0400

    fix

--- allowedOrigins.js
+++ allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     //http://localhost:3000
     //https://lukeblog.onrender.com
-    'http://localhost:3000'
+    'https://lukeblog.onrender.com'
 ]
 
 module.exports = allowedOrigins
